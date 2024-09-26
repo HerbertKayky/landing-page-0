e182de3bced91d9a14741bbca193bd081891b9b5
HerbertKayky <herbertkayky9@gmail.com>
2024-09-26 19:58:03 -0300

    adicionado responsividade ao componente about

--- src/app/components/about/index.tsx
+++ src/app/components/about/index.tsx
@@ -8,7 +8,7 @@ export function About() {
     <div>
       <div className="w-full h-4 bg-green-dark" id="sobre"></div>
       <div className="bg-green-new">
-        <div className="flex justify-center items-center py-10">
+        <div className="flex justify-center items-center py-10 flex-wrap">
           <div className="m-10">
             <Image
               className="rounded-lg"
@@ -20,7 +20,7 @@ export function About() {
             />
           </div>
 
-          <div className="max-w-4xl pr-4">
+          <div className="max-w-4xl px-3">
             <h1 className="text-yellow text-5xl">Ana Alícia</h1>
             <p className="text-gray-200 max-w-xl mt-6">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. In, vel
@@ -28,14 +28,14 @@ export function About() {
               fuga quaerat repellat perferendis! Dolorum praesentium itaque quis
               ea suscipit impedit? Lorem ipsum dolor sit amet consectetur
               adipisicing elit. In, vel dignissimos animi dignissimos animi quis
-              neque ratione rerum labore facere magni fuga quaerat repellat
+              neque ratione rerum labore facere magni fuga quaerat repellat.
             </p>
             <p className="text-gray-200 max-w-xl mt-6">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
               expedita ipsa minima quidem harum iste ratione soluta accusamus
               itaque consectetur. Odio sed eos quis pariatur natus consequatur,
               est sequi ut. Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit. Veniam expedita ipsa minima
+              elit. Veniam expedita ipsa minima.
             </p>
             <div className="inline-flex items-center px-6 py-2 rounded-lg bg-yellow gap-3 mt-10 transition-transform hover:scale-105">
               <Image
