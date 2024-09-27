3bb272e9bcb9764e4239ef2ba795ccb7ba88c197
HerbertKayky <herbertkayky9@gmail.com>
2024-09-26 21:46:04 -0300

    criado componente layer com frase

---
+++ src/app/components/layer/index.tsx
@@ -0,0 +1,13 @@
+export function Layer() {
+  return (
+    <div className="bg-gradient flex flex-col items-center px-4 py-12">
+      <div className="text-center mb-5">
+        <h1 className="text-3xl text-slate-700 leading-relaxed">
+          O resultado da terapia é tornar-se uma pessoa <br /> autônoma, capaz
+          de ser o que é e de escolher o seu <br /> caminho." -{" "}
+          <span className="italic text-quase">Carl Rogers</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/about";
 import { Header } from "./components/header";
 import { Hero } from "./components/hero";
+import { Layer } from "./components/layer";
 import { Services } from "./components/services";
 import { Specialties } from "./components/specialties/couple";
 import { SpecialtiesIndividual } from "./components/specialties/individual";
@@ -14,6 +15,7 @@ export default function Home() {
       <Specialties />
       <SpecialtiesIndividual />
       <About />
+      <Layer />
     </>
   );
 }
